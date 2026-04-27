cd08ba7fe92afa0d866b4fdb25651989178a2cc9
MuhammadsolihGofurov <gofurovmuhammadsolih004@gmail.com>
2026-04-27 23:12:04 +0500

    add Doctor.java entity

---
+++ src/main/java/java_database_capstone/Entity/Doctor.java
@@ -0,0 +1,32 @@
+package java_database_capstone.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Table(name = "doctors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String specialty;
+
+    @ElementCollection
+    @CollectionTable(name = "doctor_available_times", joinColumns = @JoinColumn(name = "doctor_id"))
+    @Column(name = "available_time")
+    private List<String> availableTimes;
+
+    // Agar Lombok ishlatmasangiz, Getter va Setterlarni bu yerga qo'shing
+}
